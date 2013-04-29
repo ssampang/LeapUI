d9ed240026baf45c62a624992fc4e238ec0bc3c3
Siddarth Sampangi <siddarthsampangi@gmail.com>
2013-04-28 17:33:27 -0700

    Initial Commit

---
+++ LeapUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LeapUI
+//
+//  Created by Siddarth Sampangi on 28/04/2013.
+//  Copyright (c) 2013 Siddarth Sampangi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ LeapUI/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  LeapUI
+//
+//  Created by Siddarth Sampangi on 28/04/2013.
+//  Copyright (c) 2013 Siddarth Sampangi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
